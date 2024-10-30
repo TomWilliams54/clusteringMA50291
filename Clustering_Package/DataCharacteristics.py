df97ad8218aefc368724812719f3548d6e469a37
Tom Williams <tlw54@bath.ac.uk>
2024-10-30 16:29:19 +0000

    Test, created clustering package

---
+++ Clustering_Package/DataCharacteristics.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def data_init(Filename)
+    
+    data = pd.read_csv(str(Filename))
+    # Calculating mean
+    mean = data.mean()
+
+    # Calculating median
+    med = data.median()
+
+    # Calculating variance
+    var = data.var()
+
+    # Calculating standard deviation
+    std_dev = data.std()
+
+    return data,mean,med,var,std_dev
